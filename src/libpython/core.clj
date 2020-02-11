f3469b9935c2e141c04600d0c3c0bd9536ec3d31
Lars Kristian Maron Telle <larskristian.marontelle@gmail.com>
2020-02-11 19:17:24 +0100

    Add prediction of next word

--- src/libpython/core.clj
+++ src/libpython/core.clj
@@ -12,4 +12,23 @@
 ;; encode text input
 (def indexed-tokens (py/$a tokenizer encode text))
 ;; convert indexed-tokens to PyTorch tensor
-(def token-tensor (torch/tensor [indexed-tokens]))
+(def tokens-tensor (torch/tensor [indexed-tokens]))
+;; load pre-trained model (weights)
+(def model (py/$a transformers/GPT2LMHeadModel from_pretrained "gpt2"))
+
+;; Set the model in evaluation mode to deactivate the DropOut modules.
+;; IMPORTANT: This is to have reproducible results during evaluation.
+(py/$a model eval)
+
+;; predict all tokens
+(def predictions (py/with [r (torch/no_grad)]
+                          (first (model tokens-tensor))))
+
+;; get the predicted next sub-word
+(def predicted-token (let [last-word-predictions (-> predictions first last)
+                           arg-max (torch/argmax last-word-predictions)]
+                       (py/$a arg-max item)))
+
+;; decode indexed-tokens + predicted-token
+(py/$a tokenizer decode (-> (into [] indexed-tokens)
+                           (conj predicted-token)))
