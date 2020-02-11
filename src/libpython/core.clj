d80c59b1b98c3b70d5b56c2120de13dee8b105bc
Lars Kristian Maron Telle <larskristian.marontelle@gmail.com>
2020-02-11 18:18:19 +0100

    Add Python libraries to Clojure

--- src/libpython/core.clj
+++ src/libpython/core.clj
@@ -1,6 +1,11 @@
-(ns libpython.core)
+(ns libpython.core
+  (:require [libpython-clj.require :refer [require-python]]
+            [libpython-clj.python :as py]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(require-python '[transformers :as transformers])
+(require-python '[torch :as torch])
+
+(def tokenizer (py/$a transformers/GPT2Tokenizer from_pretrained "gpt2"))
+(def text "Who was Jim Henson? Jim henson was a")
+(def indexed-tokens (py/$a tokenizer encode text))
+(def token-tensor (torch/tensor [indexed-tokens]))
