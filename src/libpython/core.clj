89e8c1fe699f42250287a715db2500a954089619
Lars Kristian Maron Telle <larskristian.marontelle@gmail.com>
2020-02-11 19:05:08 +0100

    Add descriptive comments

--- src/libpython/core.clj
+++ src/libpython/core.clj
@@ -2,10 +2,14 @@
   (:require [libpython-clj.require :refer [require-python]]
             [libpython-clj.python :as py]))
 
+;; load Python libraries
 (require-python '[transformers :as transformers])
 (require-python '[torch :as torch])
 
-(def tokenizer (py/$a transformers/GPT2Tokenizer from_pretrained "gpt2"))
 (def text "Who was Jim Henson? Jim henson was a")
+;; load pre-trained model tokenizer (vocabulary)
+(def tokenizer (py/$a transformers/GPT2Tokenizer from_pretrained "gpt2"))
+;; encode text input
 (def indexed-tokens (py/$a tokenizer encode text))
+;; convert indexed-tokens to PyTorch tensor
 (def token-tensor (torch/tensor [indexed-tokens]))
