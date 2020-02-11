91299841ecbea0f961bab0e03819ca73bfb07bf0
Lars Kristian Maron Telle <larskristian.marontelle@gmail.com>
2020-02-11 19:23:35 +0100

    Add prediction of multiple words

--- src/libpython/core.clj
+++ src/libpython/core.clj
@@ -16,6 +16,9 @@
 ;; load pre-trained model (weights)
 (def model (py/$a transformers/GPT2LMHeadModel from_pretrained "gpt2"))
 
+(def generated (into [] (py/$a tokenizer encode "The Manhatten bridge")))
+(def context (torch/tensor [generated]))
+
 ;; Set the model in evaluation mode to deactivate the DropOut modules.
 ;; IMPORTANT: This is to have reproducible results during evaluation.
 (py/$a model eval)
@@ -32,3 +35,32 @@
 ;; decode indexed-tokens + predicted-token
 (py/$a tokenizer decode (-> (into [] indexed-tokens)
                            (conj predicted-token)))
+
+(defn generate-sequence-step
+  [{:keys [generated-tokens context past]}]
+  (let [[output past] (model context :past past)
+        token (-> (torch/argmax (first output)))
+        new-generated  (conj generated-tokens (py/$a token tolist))]
+    {:generated-tokens new-generated
+     :context (py/$a token unsqueeze 0)
+     :past past
+     :token token}))
+
+(defn decode-sequence
+  [{:keys [generated-tokens]}]
+  (py/$a tokenizer decode generated-tokens))
+
+(defn generate-text [starting-text num-of-words-to-predict]
+  (let [tokens (into [] (py/$a tokenizer encode starting-text))
+        context (torch/tensor [tokens])
+        result (reduce
+                (fn [r i]
+                  (println i) ;; prediction indicator
+                  (generate-sequence-step r))
+
+                {:generated-tokens tokens
+                 :context context
+                 :past nil}
+
+                (range num-of-words-to-predict))]
+    (decode-sequence result)))
